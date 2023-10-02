e52d725943d9af4fdccc9dece43f0a99ab421b35
SriramKishorePalepu <146732846+SriramKishorePalepu@users.noreply.github.com>
2023-10-02 23:49:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 async def defaultRoute():
     return {"message": "Please update the url and include /stockTicker/ \"Name of the company\" e.g. /stockTicker/tsla and press enter"}
 
-@app.get("/stockTicker/{stockTkr}")
-async def root(stockTkr):
-    stock = yf.Ticker(stockTkr)
+@app.get("/currentstockprice/{stockTkr}")
+async def getCurrentStockPriceUsingStockTicker(stockTicker : str):
+    stock = yf.Ticker(stockTicker)
     price = stock.info['currentPrice']
     stockLongName = stock.info['longName']
     return {"message": "Stock price of " + stockLongName + " is "+ str(price)}
